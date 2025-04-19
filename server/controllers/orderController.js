4415d1020cb2c13f2d6693985dc8c887c6585ec9
Mohammad rafay <rafayshabbir42@gmail.com>
2025-04-19 15:06:04 +0500

    order controller change

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -40,30 +40,33 @@ return res.json({
 }
 
 // get user By Id:/api/order/user
+export const getUserOrders = async (req,res)=>{
+try {
+    const {userId} = req.body
 
-export const getUserOrders = async (req, res) => {
-  try {
-    const orders = await Order.find({ userId: req.userId })
-      .populate('items.product') // Populate product details if needed
-      .sort({ createdAt: -1 }); // Newest orders first
+    const orders = await Order.find({
+        userId,
+        $or:[{paymentType:'COD'},{isPaid:true}]
+    }).populate("items.product address").sort({
+        createdAt:-1
+    })
+    res.json({success:true,orders})
 
+} catch (error) {
     res.json({
-      success: true,
-      orders
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching orders'
-    });
-  }
-};
+    success:false,
+    message:error.message
+})
+}
+}
 
 // Get all Orders(for saller/admin):/api/order/seller
 export const getAllOrders = async (req,res)=>{
 try {
+    const {userId} = req.body
+
     const orders = await Order.find({
+      userId,
         $or:[{paymentType:'COD'},{isPaid:true}]
     }).populate("items.product address").sort({
         createdAt:-1
